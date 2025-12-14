c225d27894240f36e63bf20a624fdb73c93d5528
askarbatcha <askarbatcha160@gmail.com>
2025-12-14 22:57:45 +0530

    Create Greatest among three numbers.java

---
+++ Greatest among three numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class GreatestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a >= b && a >= c)
+            System.out.println("Greatest: " + a);
+        else if (b >= a && b >= c)
+            System.out.println("Greatest: " + b);
+        else
+            System.out.println("Greatest: " + c);
+    }
+}
