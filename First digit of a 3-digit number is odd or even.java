bba17bfdc33659785def27dafc572e817b05df90
askarbatcha <askarbatcha160@gmail.com>
2025-12-14 23:01:40 +0530

    Create First digit of a 3-digit number is odd or even.java

---
+++ First digit of a 3-digit number is odd or even.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class FirstDigitOddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int first = n / 100;
+        if (first % 2 == 0)
+            System.out.println("First digit is Even");
+        else
+            System.out.println("First digit is Odd");
+    }
+}
